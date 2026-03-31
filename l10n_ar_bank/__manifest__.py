1b5c9e7513a165a51d8623a1489460d76441deb7
root <root@HP-EliteBook-840-G3-6fbd8533.localdomain>
2026-03-31 16:05:17 -0300

    Agregando módulos locales a v19

---
+++ l10n_ar_bank/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Listado de Bancos Argentinos",
+    "version": "19.0.1.1.0",
+    "category": "Localization/Argentina",
+    "sequence": 14,
+    "author": "ADHOC SA",
+    "license": "AGPL-3",
+    "summary": "",
+    "depends": [
+        "base",
+        "l10n_ar",
+    ],
+    "data": ["data/res_bank.xml", "views/res_bank_view.xml"],
+    "installable": True,
+    "auto_install": True,
+    "application": False,
+}
