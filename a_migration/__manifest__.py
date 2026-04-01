5a9d2f94e9bc221312dfb243cd9856419d90e4fe
Diego Kleiner <diegokleiner@HP-EliteBook-840-G3-6fbd8533.localdomain>
2026-04-01 20:57:28 -0300

    [CRACK]

--- a_migration/__manifest__.py
+++
@@ -1,8 +0,0 @@
-{
-    'name': 'Force Migration Cleanup',
-    'version': '19.0.1.0',
-    'author': 'Diego Kleiner',
-    'depends': ['base'],
-    'installable': True,
-    'auto_install': True, # Esto hace que se intente instalar solo
-}
