ec63b854bd1190f18804d8a02e6fa73d23592164
Diego Kleiner <diegokleiner@HP-EliteBook-840-G3-6fbd8533.localdomain>
2026-04-01 20:40:19 -0300

    Add forced migration module a_migration

---
+++ a_migration/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Force Migration Cleanup',
+    'version': '19.0.1.0',
+    'author': 'Diego Kleiner',
+    'depends': ['base'],
+    'installable': True,
+    'auto_install': True, # Esto hace que se intente instalar solo
+}
