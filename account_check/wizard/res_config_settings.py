f2837bc83ca0ad2694b539f38096761b8f244631
Diego Kleiner <diegokleiner@HP-EliteBook-840-G3-6fbd8533.localdomain>
2026-04-01 04:01:42 -0300

    [INICIO] Iniciando rama 16.0 (5)

--- account_check/wizard/res_config_settings.py
+++ account_check/wizard/res_config_settings.py
@@ -6,20 +6,14 @@ class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
     rejected_check_account_id = fields.Many2one(
-        'account.account',
         related='company_id.rejected_check_account_id',
-        string='Rejected Checks Account',
         readonly=False,
     )
     deferred_check_account_id = fields.Many2one(
-        'account.account',
         related='company_id.deferred_check_account_id',
-        string='Deferred Checks Account',
         readonly=False,
     )
     holding_check_account_id = fields.Many2one(
-        'account.account',
         related='company_id.holding_check_account_id',
-        string='Holding Checks Account',
         readonly=False,
     )
