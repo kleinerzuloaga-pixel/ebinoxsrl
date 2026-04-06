be5e1d17b6049ee67fee33bfee9758a792a53c81
Diego Kleiner <diegokleiner@HP-EliteBook-840-G3-6fbd8533.localdomain>
2026-04-06 20:22:05 -0300

    [16.0 EMPTY]

---
+++ my_custom_module/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Ebinox Custom',
+    'version': '19.0.1.0.0',
+    'author': 'Kleiner Zuloaga',
+    'website': 'https://www.kleinerzuloaga.com',
+    'license': 'LGPL-3',
+    'category': 'Customization',
+    'depends': ['base'],
+    'installable': True,
+    'application': False,
+}
