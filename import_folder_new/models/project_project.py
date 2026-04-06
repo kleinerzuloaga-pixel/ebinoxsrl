90bdab95637794cb091ee1196f2a690eb7235273
Diego Kleiner <diegokleiner@HP-EliteBook-840-G3-6fbd8533.localdomain>
2026-04-06 20:19:11 -0300

    [16.0 EMPTY]

--- import_folder_new/models/project_project.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api, exceptions, _
-
-class ProjectInherit(models.Model):
-    _inherit = 'project.project'
-
-    importation = fields.Boolean(string='Importación' , default=False)
