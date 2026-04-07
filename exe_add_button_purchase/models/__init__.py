034ca36f73902269be2d46eeb67c2dd21b0ade21
Diego Kleiner <diegokleiner@HP-EliteBook-840-G3-6fbd8533.localdomain>
2026-04-06 22:25:13 -0300

    [16.0 EMPTY 4APR]

--- exe_add_button_purchase/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import exe_add_button_purchase
--- l10n_ar_account_inflation_adjust/models/__init__.py
+++
@@ -1,6 +0,0 @@
-#from . import account_move_line
-#from . import account_chart_template
-#from . import account_journal
-from . import account_account
-from . import inflation_adjustment_index
-#from . import account_tax
